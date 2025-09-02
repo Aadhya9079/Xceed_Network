d5f3426699ee9d37b9e684a9dba8211cc088934d
Aadhya9079 <146864352+Aadhya9079@users.noreply.github.com>
2025-09-02 12:07:48 +0530

    Update JoinUs.jsx

--- src/components/JoinUs.jsx
+++ src/components/JoinUs.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import "./JoinUs.css"
+import "./joinus.css"
 import { FaLinkedinIn, FaFacebookF, FaInstagram } from "react-icons/fa"
 import { FaXTwitter } from "react-icons/fa6"
 
